8f54698508825fdd3def14a94250086a0130f27c
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2021-02-24 14:36:59 -0500

    Add divisible Sum Pairs challenge

--- warmup.js
+++ warmup.js
@@ -177,4 +177,28 @@ function birthday(s, d, m) {
   return (possibleAnswers);
 }
 
-console.log(birthday([1, 2, 1, 3, 2], 3, 2))
+console.log(birthday([1, 2, 1, 3, 2], 3, 2));
+
+console.log('Divisible Sum Pairs');
+
+function divisibleSumPairs(n, k, ar) {
+  let count = 0;
+  ar = ar.sort((a, b) => a - b);
+  // console.log(ar)
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 1; j < n; j++) {
+      if( (ar[i] < ar[j]) && ( (ar[i] +  ar[j]) % k) === 0) {
+        // console.log(`${ar[i]} + ${ar[j]} = ${ar[i] + ar[j]}`)
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+let divisibleSumPairsArr = '64 95 76 35 83 2 97 21 26 6 20 98 100 79 84 40 62 62 20 9 87 40 47 38 12 10 92 68 4 34 98 53 15 4 9 4 77 60 36 49 10 73 56 43 31 6 18 91 53 73 28 90 79 3 72 64 29 89 95 28 23 7 54 23 34 90 100 73 77 98 95 89 22 42 98 39 92 25 44 40 7 100 62 28 25 21 64 55 84 54 99 14 5 51 34 66 4 5 49 56'.split(' ');
+divisibleSumPairsArr = divisibleSumPairsArr.map((numStr) => parseInt(numStr));
+let divisibleSumPairsFactor = 21;
+let divisibleSumPairsArrLength = divisibleSumPairsArr.length;
+
+console.log(divisibleSumPairs(divisibleSumPairsArrLength, divisibleSumPairsFactor, divisibleSumPairsArr));
